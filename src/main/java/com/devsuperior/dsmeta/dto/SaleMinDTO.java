1b6c0fb42fbd4b7fcf839eecac129667e0a9f67f
Luciano Nascimento <luciano.m.nasc@gmail.com>
2024-08-12 22:09:50 -0300

    first commit

---
+++ src/main/java/com/devsuperior/dsmeta/dto/SaleMinDTO.java
@@ -0,0 +1,17 @@
+package com.devsuperior.dsmeta.dto;
+
+import java.time.LocalDate;
+
+import com.devsuperior.dsmeta.entities.Sale;
+
+public record SaleMinDTO (
+			Long id,
+			Double amount,
+			LocalDate date
+
+){
+	public SaleMinDTO(Sale entity) {
+		this(entity.getId(), entity.getAmount(), entity.getDate());
+	}
+}
+
